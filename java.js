18b801f61d00d1915ded42d94f9d45f4a7a69e10
deepak96rana <89519524+deepak96rana@users.noreply.github.com>
2021-11-13 13:58:58 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+function togglenav(){
+    document.getElementById("sidebar").classList.toggle("active");
+}
+function openForm() {
+    document.getElementById("myform").style.display = "flex";
+  }
+  function closeForm() {
+    document.getElementById("myform").style.display = "none";
+  }
